7b345480980e6516865e7a1fbc189c2af1b70c58
Lukasz <intergenixyt@gmail.com>
2026-07-14 19:15:25 +0200

    simplify

--- android/app/src/main/java/com/genix/myapplication/MainActivity.kt
+++ android/app/src/main/java/com/genix/myapplication/MainActivity.kt
@@ -6,42 +6,31 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.genix.myapplication.ui.theme.MyApplicationTheme
 
+// TODO(genix): passing data around
+// TODO(genix): multiple modules (umbrella pattern)
+// TODO(genix): activity
+// TODO(genix): fragment
+// TODO(genix): navigation
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyApplicationTheme {
+            MaterialTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    Text(
+                        text = "Hello",
+                        Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
     }
 }
